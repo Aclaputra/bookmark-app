6fb8b5cebe904704463f407bf2cfe04c22f1f570
Acla Putra <muhamadacla22@gmail.com>
2021-05-07 00:59:03 +0700

    Front end setup with Inertia, React and Typescript

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+// use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+// use Inertia\Inertia;
 
 class HomeController extends Controller
 {
@@ -23,6 +27,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return Inertia::render('Home/index');
     }
 }
