508fc0952fa20bef8d9633cfce0d7c6c40c05ce6
Acla Putra <muhamadacla22@gmail.com>
2021-05-08 12:25:48 +0700

    Bookmark add form - Modified resources/js/Pages/Bookmark/Add/index.tsx

--- resources/js/Pages/Bookmark/Add/index.tsx
+++ resources/js/Pages/Bookmark/Add/index.tsx
@@ -1,15 +1,37 @@
-import React from "react"
+import { Inertia } from "@inertiajs/inertia"
+import React, { useState } from "react"
 
 interface Props {}
 
 const BookmarkAddPage: React.FC<Props> = () => {
+  const [state, setstate] = useState({
+    link: "",
+    title: "some hardcoded title",
+  })
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setstate({
+      ...state,
+      [event.currentTarget.name]: event.currentTarget.value,
+    })
+  }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    Inertia.post("/bookmark/preview", state)
+  }
   return (
     <div className="row">
       <div className="col-sm-8 mx-auto">
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <div className="form-group">
-            <label htmlFor="Link"></label>
-            <input type="text" className="form-control" name="link" />
+            <label htmlFor="link">Link</label>
+            <input
+              type="text"
+              className="form-control"
+              name="link"
+              onChange={handleChange}
+            />
           </div>
         </form>
       </div>
