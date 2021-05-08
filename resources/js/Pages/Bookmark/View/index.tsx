c7ea6b2fe86ab56b587204e949000b9839e669ae
Acla Putra <muhamadacla22@gmail.com>
2021-05-08 17:53:44 +0700

    Handling the save through React form component -> added resources/js/Pages/Bookmark/View/

---
+++ resources/js/Pages/Bookmark/View/index.tsx
@@ -0,0 +1,42 @@
+import React from "react"
+import Layout from "../../../components/common/layout"
+import { Inertia } from "@inertiajs/inertia"
+
+interface Props {
+  bookmark: any
+}
+
+const BookmarkViewPage: React.FC<Props> = ({ bookmark }) => {
+  const handleSave = (event: React.MouseEvent) => {
+    event.preventDefault()
+    Inertia.post("/bookmark/make-active", { id: bookmark.id })
+  }
+  return (
+    <Layout>
+      <div className="row">
+        <div className="col-md-6">
+          {bookmark.title && (
+            <div className="card">
+              <div className="card-header">{bookmark.title}</div>
+              <div className="card-body">
+                <p>{bookmark.description}</p>
+                <div className="mb-3">
+                  <img
+                    src={bookmark.img_url}
+                    alt={bookmark.title}
+                    width="100%"
+                  />
+                </div>
+                <button className="btn btn-primary" onClick={handleSave}>
+                  Save
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export default BookmarkViewPage
