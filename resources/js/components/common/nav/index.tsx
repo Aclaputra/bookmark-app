52433aabbfe875be9c588f06e646edd3fc1937a1
Acla Putra <muhamadacla22@gmail.com>
2021-05-11 18:10:19 +0700

    setting up ziggy routes -> modified resources/js/components/common/nav/index.tsx

--- resources/js/components/common/nav/index.tsx
+++ resources/js/components/common/nav/index.tsx
@@ -1,4 +1,6 @@
+import { InertiaLink } from "@inertiajs/inertia-react"
 import React from "react"
+import route from "ziggy-js"
 
 const Menu = () => {
   return (
@@ -22,9 +24,20 @@ const Menu = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <InertiaLink
+                className="nav-link"
+                href={route("bookmark.index").url()}
+              >
                 Bookmarks
-              </a>
+              </InertiaLink>
+            </li>
+            <li className="nav-item">
+              <InertiaLink
+                className="nav-link"
+                href={route("bookmark.add").url()}
+              >
+                Bookmark Add
+              </InertiaLink>
             </li>
           </ul>
 
